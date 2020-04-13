9690c3612f8ee129fb6f7e3641c66bd49d31178a
Monika <monikasharma26081991@gmail.com>
2020-04-13 19:35:33 -0400

    [Monika] Packages added

--- app/src/main/java/com/example/c0767722_w2020_mad3125_fp/Actvities/MainActivity.java
+++ app/src/main/java/com/example/c0767722_w2020_mad3125_fp/Actvities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.c0767722_w2020_mad3125_fp;
+package com.example.c0767722_w2020_mad3125_fp.Actvities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.c0767722_w2020_mad3125_fp.R;
 
 public class MainActivity extends AppCompatActivity {
 
