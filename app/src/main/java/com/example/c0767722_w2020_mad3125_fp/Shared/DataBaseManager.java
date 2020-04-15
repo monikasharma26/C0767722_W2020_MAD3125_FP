9ddb3a1e91c370d23de7d7c73b43387afdf425ff
Monika <monikasharma26081991@gmail.com>
2020-04-14 21:30:34 -0400

    [Monika] DatabaseManager model class method added

--- app/src/main/java/com/example/c0767722_w2020_mad3125_fp/Shared/DataBaseManager.java
+++ app/src/main/java/com/example/c0767722_w2020_mad3125_fp/Shared/DataBaseManager.java
@@ -1,4 +1,45 @@
 package com.example.c0767722_w2020_mad3125_fp.Shared;
 
-public class DataBaseManager {
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+import com.example.c0767722_w2020_mad3125_fp.Models.LoginDetails;
+
+import java.util.ArrayList;
+
+public class DataBaseManager extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "users";
+    private static final int DATABASE_VERSION = 1;
+
+
+    private static String tableName;
+    private static String tableCreatorString;
+
+    public DataBaseManager(Context context)
+    {
+        super(context, DATABASE_NAME , null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //create the table
+        db.execSQL(tableCreatorString);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //drop table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + tableName);
+        //recreate the table
+        onCreate(db);
+    }
+
+    
+
 }
