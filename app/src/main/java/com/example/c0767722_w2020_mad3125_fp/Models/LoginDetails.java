3baf12e1298f15de7591e50cef548d310d71b944
Monika <monikasharma26081991@gmail.com>
2020-04-14 21:27:02 -0400

    [Monika] LOGINDETAILS properties added

--- app/src/main/java/com/example/c0767722_w2020_mad3125_fp/Models/LoginDetails.java
+++ app/src/main/java/com/example/c0767722_w2020_mad3125_fp/Models/LoginDetails.java
@@ -1,4 +1,27 @@
 package com.example.c0767722_w2020_mad3125_fp.Models;
 
+
 public class LoginDetails {
+    private String id;
+    private String loginTime;
+    public LoginDetails() {
+    }
+    public LoginDetails(String id,String LoginTime ) {
+
+        this.id=id;
+        this.loginTime = LoginTime;
+    }
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {this.id = id; }
+
+    public String getLoginTime() {
+        return loginTime;
+    }
+
+    public void setLoginTime(String lTime) {this.loginTime = lTime; }
 }
+
+
