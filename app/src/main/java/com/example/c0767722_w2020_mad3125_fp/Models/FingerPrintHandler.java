d5ba1c8bf3966220e39796a85e8eede169a8f342
Monika <monikasharma26081991@gmail.com>
2020-04-16 17:43:24 -0400

    [Monika] FingerPrintHAndler class Added

--- app/src/main/java/com/example/c0767722_w2020_mad3125_fp/Models/FingerPrintHandler.java
+++ app/src/main/java/com/example/c0767722_w2020_mad3125_fp/Models/FingerPrintHandler.java
@@ -1,8 +1,16 @@
 package com.example.c0767722_w2020_mad3125_fp.Models;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.fingerprint.FingerprintManager;
 import android.os.CancellationSignal;
+import android.widget.TextView;
+
+import androidx.core.content.ContextCompat;
+
+import com.example.c0767722_w2020_mad3125_fp.Actvities.DashBoardActivity;
+import com.example.c0767722_w2020_mad3125_fp.R;
 
 public class FingerPrintHandler extends FingerprintManager.AuthenticationCallback {
     private Context context;
@@ -17,7 +25,36 @@ public class FingerPrintHandler extends FingerprintManager.AuthenticationCallbac
 
     @Override
     public void onAuthenticationError(int errorCode, CharSequence errString) {
-       
+        this.updateText("There was an Error "+ errString,false);
+    }
+    @Override
+    public void onAuthenticationFailed() {
+        this.updateText("Authentication Failed. ", false);
+    }
+    @Override
+    public void onAuthenticationHelp(int helpCode, CharSequence helpString) {
+        this.updateText("Error "+helpString,false);
+    }
+
+    @Override
+    public void onAuthenticationSucceeded(FingerprintManager.AuthenticationResult result) {
+        this.updateText("Authentication SuccessFull",true);
+    }
+
+
+
+    private void updateText(String s, boolean b) {
+        TextView fingerprintMessage = ((Activity)context).findViewById(R.id.fingerprintMessage);
+        fingerprintMessage.setText(s);
+        if(b == false){
+            fingerprintMessage.setTextColor(ContextCompat.getColor(context,R.color.colorAccent));
+        }else{
+            fingerprintMessage.setTextColor(ContextCompat.getColor(context,R.color.colorAccent));
+            ((Activity)context).setIntent(new Intent(context, DashBoardActivity.class));
+            context.startActivity(new Intent(context,DashBoardActivity.class));
+            context.startActivity(new Intent(context,DashBoardActivity.class));
+            ((Activity) context).finish();
+        }
     }
 
 }
