a33a2cae0ba1c54494d32a6513372b1a9cd0f3f7
Monika <monikasharma26081991@gmail.com>
2020-04-16 00:05:03 -0400

    [Monika] SignUpActivity Added

---
+++ app/src/main/java/com/example/c0767722_w2020_mad3125_fp/Actvities/SignUpActivity.java
@@ -0,0 +1,14 @@
+package com.example.c0767722_w2020_mad3125_fp.Actvities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
