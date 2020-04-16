b64e285dc150e79e03d259239070f2041ffedeb0
Monika <monikasharma26081991@gmail.com>
2020-04-16 17:05:47 -0400

    [Monika] DashboardActivity Addded

---
+++ app/src/main/java/com/example/c0767722_w2020_mad3125_fp/Actvities/DashBoardActivity.java
@@ -0,0 +1,16 @@
+package com.example.c0767722_w2020_mad3125_fp.Actvities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.c0767722_w2020_mad3125_fp.R;
+
+public class DashBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dash_board);
+    }
+}
