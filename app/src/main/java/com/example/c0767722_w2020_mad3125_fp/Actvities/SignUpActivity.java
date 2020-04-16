6a90878d39b5f33bd7ce76bc27536a1d57f06f91
Monika <monikasharma26081991@gmail.com>
2020-04-16 15:34:04 -0400

    [Monika] COntyrols defined

--- app/src/main/java/com/example/c0767722_w2020_mad3125_fp/Actvities/SignUpActivity.java
+++ app/src/main/java/com/example/c0767722_w2020_mad3125_fp/Actvities/SignUpActivity.java
@@ -1,18 +1,35 @@
 package com.example.c0767722_w2020_mad3125_fp.Actvities;
 
+import android.os.Bundle;
+import android.widget.EditText;
+
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
+import androidx.appcompat.widget.AppCompatTextView;
 
 import com.example.c0767722_w2020_mad3125_fp.R;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 public class SignUpActivity extends AppCompatActivity {
+
+    @InjectView(R.id.email)
+    EditText email;
+    @InjectView(R.id.passwrd)
+    EditText passwrd;
+    @InjectView(R.id.confPasword)
+    EditText confPasword;
+    @InjectView(R.id.btnSignUp)
+    AppCompatTextView btnSignUp;
+    @InjectView(R.id.btnCancel)
+    AppCompatTextView btnCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ButterKnife.inject(this);
         ActionBar bar = getSupportActionBar();
         bar.hide();
     }
