3261badbef05d1dd1621f7d715d1b9dfa1eb5710
Monika <monikasharma26081991@gmail.com>
2020-04-16 00:39:56 -0400

    [Monika] Login button and signup functipn added

--- app/src/main/java/com/example/c0767722_w2020_mad3125_fp/Actvities/HomeActivity.java
+++ app/src/main/java/com/example/c0767722_w2020_mad3125_fp/Actvities/HomeActivity.java
@@ -1,16 +1,44 @@
 package com.example.c0767722_w2020_mad3125_fp.Actvities;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.example.c0767722_w2020_mad3125_fp.R;
+import com.google.android.material.button.MaterialButton;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    @InjectView(R.id.btnLogin)
+    MaterialButton btnLogin;
+    @InjectView(R.id.btnSignUp)
+    MaterialButton btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.inject(this);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
