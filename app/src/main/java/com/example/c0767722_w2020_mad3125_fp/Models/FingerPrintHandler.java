83a6c15d34c372b9765b16fd76d426a90e61a4eb
Monika <monikasharma26081991@gmail.com>
2020-04-16 17:08:06 -0400

    [Monika] Methods override for Authenticationcallback of fingerPrint

--- app/src/main/java/com/example/c0767722_w2020_mad3125_fp/Models/FingerPrintHandler.java
+++ app/src/main/java/com/example/c0767722_w2020_mad3125_fp/Models/FingerPrintHandler.java
@@ -1,4 +1,24 @@
 package com.example.c0767722_w2020_mad3125_fp.Models;
 
-public class FingerPrintHandler {
+import android.content.Context;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.CancellationSignal;
+
+public class FingerPrintHandler extends FingerprintManager.AuthenticationCallback {
+    private Context context;
+    CancellationSignal cancellationSignal = new CancellationSignal();
+    public FingerPrintHandler(Context context) {
+        this.context = context;
+    }
+    public void startAuth(FingerprintManager fingerprintManager,FingerprintManager.CryptoObject cryptoObject){
+        CancellationSignal cancellationSignal =  new CancellationSignal();
+        fingerprintManager.authenticate(cryptoObject,cancellationSignal,0,this,null);
+    }
+
+    @Override
+    public void onAuthenticationError(int errorCode, CharSequence errString) {
+       
+    }
+
 }
+
