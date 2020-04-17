f7a735e982e416e29d9e3cd4b62ba15346e82750
Monika <monikasharma26081991@gmail.com>
2020-04-17 17:59:02 -0400

    [Monika] add_customerfragment added

--- app/src/main/java/com/example/c0767722_w2020_mad3125_fp/ui/AddCustomer/AddCustomerFragment.java
+++ app/src/main/java/com/example/c0767722_w2020_mad3125_fp/ui/AddCustomer/AddCustomerFragment.java
@@ -1,4 +1,39 @@
 package com.example.c0767722_w2020_mad3125_fp.ui.AddCustomer;
 
-public class AddCustomerFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.c0767722_w2020_mad3125_fp.R;
+
+
+public class AddCustomerFragment extends Fragment {
+
+    public AddCustomerFragment() {
+        // Required empty public constructor
+    }
+
+    public static AddCustomerFragment newInstance(String param1, String param2) {
+        AddCustomerFragment fragment = new AddCustomerFragment();
+        Bundle args = new Bundle();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+
+        return inflater.inflate(R.layout.fragment_addcustomer, container, false);
+    }
 }
