e07808016eea9ed47f4d5b4e2991ebfd2daa1c9f
Monika <monikasharma26081991@gmail.com>
2020-04-16 20:16:02 -0400

    [Monika] Dashboard ACtivity Updated

--- app/src/main/java/com/example/c0767722_w2020_mad3125_fp/Actvities/DashBoardActivity.java
+++ app/src/main/java/com/example/c0767722_w2020_mad3125_fp/Actvities/DashBoardActivity.java
@@ -1,16 +1,33 @@
 package com.example.c0767722_w2020_mad3125_fp.Actvities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.c0767722_w2020_mad3125_fp.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class DashBoardActivity extends AppCompatActivity {
+
+    @InjectView(R.id.bottomNavView)
+    BottomNavigationView bottomNavView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+        ButterKnife.inject(this);
+        ActionBar bar = getSupportActionBar();
+        bar.setDisplayShowTitleEnabled(true);
+        bar.setTitle("Dashboard");
+        bar.show();
+        ArrayList<BottomNavigationView> tabs = new ArrayList<>();
+
     }
 }
