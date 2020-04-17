fe5c17131440dd1da1eeaf8d05a1a395a060d7dc
Monika <monikasharma26081991@gmail.com>
2020-04-17 15:07:27 -0400

    [Monika] Logic Added for customerList

--- app/src/main/java/com/example/c0767722_w2020_mad3125_fp/Actvities/DashBoardActivity.java
+++ app/src/main/java/com/example/c0767722_w2020_mad3125_fp/Actvities/DashBoardActivity.java
@@ -1,11 +1,18 @@
 package com.example.c0767722_w2020_mad3125_fp.Actvities;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.c0767722_w2020_mad3125_fp.R;
+import com.example.c0767722_w2020_mad3125_fp.ui.ListOfCustomers.ListOfCustomersFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
@@ -17,17 +24,82 @@ public class DashBoardActivity extends AppCompatActivity {
 
     @InjectView(R.id.bottomNavView)
     BottomNavigationView bottomNavView;
-
+    ActionBar actionBar;
+    int fragmentId;
+    Menu menu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
         ButterKnife.inject(this);
-        ActionBar bar = getSupportActionBar();
-        bar.setDisplayShowTitleEnabled(true);
-        bar.setTitle("Dashboard");
-        bar.show();
-        ArrayList<BottomNavigationView> tabs = new ArrayList<>();
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle("Dashboard");
+        actionBar.show();
+        bottomNavView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                switch (menuItem.getItemId()) {
+                    case R.id.navigation_Customers:
+                        handleMenuOption(id);
+                        openFragment(ListOfCustomersFragment.newInstance("", ""));
+                       actionBar.setTitle("Customer List");
+                       return true;
+                    case R.id.navigation_Serach:
+                        handleMenuOption(id);
+                        //openFragment(ListOfCustomersFragment.newInstance("", ""));
+                        actionBar.setTitle("Search");
+                        return true;
+                    case R.id.navigation_home:
+                        handleMenuOption(id);
+                        //openFragment(ListOfCustomersFragment.newInstance("", ""));
+                        actionBar.setTitle("Home");
+                        return true;
+                    case R.id.navigation_notifications:
+                        handleMenuOption(id);
+                        //openFragment(ListOfCustomersFragment.newInstance("", ""));
+                        actionBar.setTitle("Profile");
+                        return true;
+
+                }
+
+                return true;
+                }
+            });
 
     }
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        this.menu=menu;
+        return super.onPrepareOptionsMenu(menu);
+    }
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.customer_home_page,menu);
+      if(fragmentId == 1)
+      {
+         MenuItem item = menu.findItem(R.id.btnCusAdd);
+         item.setVisible(true);
+      }
+        return true;
+    }
+    private void handleMenuOption(int id) {
+        MenuItem item = menu.findItem(R.id.btnCusAdd);
+        if (id == R.id.navigation_Customers){
+            item.setVisible(true);
+        }
+        else {
+            item.setVisible(false);
+        }
+    }
+    
+
 }
