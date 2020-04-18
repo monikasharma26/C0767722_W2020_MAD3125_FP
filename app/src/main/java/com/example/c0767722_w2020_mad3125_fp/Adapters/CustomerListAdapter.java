72040161927b3ffe0862a0141f29588dfa9eaba8
Monika <monikasharma26081991@gmail.com>
2020-04-17 21:29:07 -0400

    [Monika] Customer Adapter Constructor added

--- app/src/main/java/com/example/c0767722_w2020_mad3125_fp/Adapters/CustomerListAdapter.java
+++ app/src/main/java/com/example/c0767722_w2020_mad3125_fp/Adapters/CustomerListAdapter.java
@@ -1,4 +1,17 @@
 package com.example.c0767722_w2020_mad3125_fp.Adapters;
 
-public class CustomerListAdapter {
+import com.example.c0767722_w2020_mad3125_fp.Models.Customer;
+
+import java.util.ArrayList;
+
+public class CustomerListAdapter  {
+
+    private ArrayList<Customer> customersList;
+
+    
+    public CustomerListAdapter(ArrayList<Customer> customersList) {
+        this.customersList = customersList;
+    }
+
+
 }
