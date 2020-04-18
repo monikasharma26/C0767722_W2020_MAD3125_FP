00d85cac44f90085fb2f5601a790e37173b1ea44
Monika <monikasharma26081991@gmail.com>
2020-04-18 01:05:24 -0400

    [Monika] Customer class updated

--- app/src/main/java/com/example/c0767722_w2020_mad3125_fp/ui/ListOfCustomers/ListOfCustomersFragment.java
+++ app/src/main/java/com/example/c0767722_w2020_mad3125_fp/ui/ListOfCustomers/ListOfCustomersFragment.java
@@ -1,18 +1,26 @@
 package com.example.c0767722_w2020_mad3125_fp.ui.ListOfCustomers;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.c0767722_w2020_mad3125_fp.Adapters.CustomerListAdapter;
+import com.example.c0767722_w2020_mad3125_fp.Models.Customer;
 import com.example.c0767722_w2020_mad3125_fp.R;
 import com.example.c0767722_w2020_mad3125_fp.ui.AddCustomer.AddCustomerFragment;
 import com.google.firebase.database.DataSnapshot;
@@ -20,6 +28,10 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import butterknife.InjectView;
 
@@ -30,8 +42,11 @@ public class ListOfCustomersFragment extends Fragment {
     RecyclerView rvlistCustomers;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference("users");
+    private ArrayList<Customer> customerList;
+    private CustomerListAdapter customerAdapter;
     String userid="";
     String custId;
+    RecyclerView recyclerView;
     public ListOfCustomersFragment() {
         // Required empty public constructor
     }
@@ -92,6 +107,51 @@ public class ListOfCustomersFragment extends Fragment {
             actionBar.setTitle("List Of Customer");
         }
 
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        recyclerView = view.findViewById(R.id.rvlistCustomers);
+       //final CustomerListAdapter employeeDataAdapter = new CustomerListAdapter(getContext());
+        final CustomerListAdapter employeeDataAdapter = new CustomerListAdapter(getContext());
+        customerList = new ArrayList<>();
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                HashMap<String, HashMap<String, String>> value = (HashMap<String, HashMap<String, String>>) dataSnapshot.getValue();
+
+                HashMap<String, String>[] usersMap;
+                usersMap = value.values().toArray(new HashMap[value.size()]);
+
+                for (int i = 0; i < usersMap.length; i++) {
+                    customerList.add(new Customer(usersMap[i].get("id"), usersMap[i].get("firstName"), usersMap[i].get("lastName"), usersMap[i].get("emailId")));
+                }
+
+                //LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+              //  employeeDataAdapter.setMyaaraylist(customerList);
+                customerAdapter = new CustomerListAdapter(customerList);
+                recyclerView.setAdapter(customerAdapter);
+                //recyclerView.setLayoutManager(layoutManager);
+               LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+               recyclerView.setLayoutManager(layoutManager);
+
+                //custListProgressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("Failed to read value.", error.toException());
+            }
+        });
+
+    }
+
+
+    public void getCustomers() {
     }
 
 }
