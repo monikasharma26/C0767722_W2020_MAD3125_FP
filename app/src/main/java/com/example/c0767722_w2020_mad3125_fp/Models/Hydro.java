6139ae8d557189d436ee29f241cd7e255e1684ba
Monika <monikasharma26081991@gmail.com>
2020-04-19 15:07:07 -0400

    [Monika] Hydro class updated

--- app/src/main/java/com/example/c0767722_w2020_mad3125_fp/Models/Hydro.java
+++ app/src/main/java/com/example/c0767722_w2020_mad3125_fp/Models/Hydro.java
@@ -2,5 +2,42 @@ package com.example.c0767722_w2020_mad3125_fp.Models;
 
 import java.io.Serializable;
 
-public class Hydro implements Serializable {
+public class Hydro extends Bill {
+    String agencyName;
+    String unitsConsumed;
+
+    public Hydro(String customerId, String billId, String billDate, String billType, String billAmount, String agencyName, String unitsConsumed) {
+        super(customerId, billId, billDate, billType, billAmount);
+        this.agencyName = agencyName;
+        this.unitsConsumed = unitsConsumed;
+    }
+
+    public String getAgencyName() {
+        return agencyName;
+    }
+
+    public void setAgencyName(String agencyName) {
+        this.agencyName = agencyName;
+    }
+
+    public String getUnitsConsumed() {
+        return unitsConsumed;
+    }
+
+    public void setUnitsConsumed(String unitsConsumed) {
+        this.unitsConsumed = unitsConsumed;
+    }
+
+    @Override
+    public String toString() {
+        return "Hydro{" +
+                "agencyName='" + agencyName + '\'' +
+                ", unitsConsumed='" + unitsConsumed + '\'' +
+                ", customerId='" + customerId + '\'' +
+                ", billId='" + billId + '\'' +
+                ", billDate='" + billDate + '\'' +
+                ", billType='" + billType + '\'' +
+                ", billAmount='" + billAmount + '\'' +
+                '}';
+    }
 }
