3632228d484c47ee25a28bf0d781002d1c136783
Monika <monikasharma26081991@gmail.com>
2020-04-19 15:03:29 -0400

    [Monika] BillClass Properties and constructor defined

--- app/src/main/java/com/example/c0767722_w2020_mad3125_fp/Models/Bill.java
+++ app/src/main/java/com/example/c0767722_w2020_mad3125_fp/Models/Bill.java
@@ -1,4 +1,60 @@
 package com.example.c0767722_w2020_mad3125_fp.Models;
 
-public class Bill {
+import java.io.Serializable;
+
+public class Bill implements Serializable {
+    String customerId;
+    String billId;
+    String billDate;
+    String billType;
+    String billAmount;
+
+    public Bill(String customerId, String billId, String billDate, String billType, String billAmount) {
+        this.customerId = customerId;
+        this.billId = billId;
+        this.billDate = billDate;
+        this.billType = billType;
+        this.billAmount = billAmount;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getBillId() {
+        return billId;
+    }
+
+    public void setBillId(String billId) {
+        this.billId = billId;
+    }
+
+    public String getBillDate() {
+        return billDate;
+    }
+
+    public void setBillDate(String billDate) {
+        this.billDate = billDate;
+    }
+
+    public String getBillType() {
+        return billType;
+    }
+
+    public void setBillType(String billType) {
+        this.billType = billType;
+    }
+
+    public String getBillAmount() {
+        return billAmount;
+    }
+
+    public void setBillAmount(String billAmount) {
+        this.billAmount = billAmount;
+    }
+
 }
